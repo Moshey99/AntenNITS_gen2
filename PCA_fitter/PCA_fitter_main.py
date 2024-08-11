904cc9ec70eec9b495c4e230be7e7195f7936e60
Moshey99 <moshe14199958@gmail.com>
2024-08-12 00:59:27 +0300

    first commit for building on antenna size =(20,20)

--- PCA_fitter/PCA_fitter_main.py
+++ PCA_fitter/PCA_fitter_main.py
@@ -21,7 +21,7 @@ os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 parser = argparse.ArgumentParser(description='Convolutional AutoEncoder for Geometry Data')
 parser.add_argument('--data_path', type=str,
                     default=r'C:\Users\moshey\PycharmProjects\etof_folder_git\AntennaDesign_data\data_15000_3envs')
-parser.add_argument('--image_size', type=tuple, nargs='+', default=(144, 200), help='Size of the Image')
+parser.add_argument('--image_size', type=tuple, nargs='+', default=(20, 20), help='Size of the Image')
 parser.add_argument('--device', type=int, nargs='+', default=[2], help='Device to run the model on')
 parser.add_argument('--lrs', type=float, nargs='+', default=[0.0005], help='Learning Rates to try')
 parser.add_argument('--bs', type=int, nargs='+', default=[32], help='Batch Sizes to try')
@@ -54,9 +54,19 @@ test_imgs = all_imgs[split_index:]
 
 def binarize(img, nonmetal_threshold=0.5, feed_threshold=1.5):
     img[img < nonmetal_threshold] = 0
-    img[img >= feed_threshold] = 2
-    img[(img >= nonmetal_threshold) & (img < feed_threshold)] = 1
+    # img[img >= feed_threshold] = 2
+    img[(img >= nonmetal_threshold)] = 1
     return img
+
+
+def plot_explained_variance(pca: PCA):
+    explained_variance = pca.explained_variance_ratio_
+    explained_variance_cumulative = np.cumsum(explained_variance)
+    plt.plot(explained_variance_cumulative)
+    plt.title('Cumulative Explained Variance')
+    plt.xlabel('Number of Components')
+    plt.ylabel('Explained Variance')
+    plt.show()
 
 
 # DataLoader Function
@@ -71,7 +81,7 @@ class Imageprep(torch.utils.data.Dataset):
 
     def __getitem__(self, index):
         path = self.paths[index]
-        image = np.load(path)
+        image = binarize(np.load(path))
         image = cv2.resize(image, (image_size[1], image_size[0]))
         image = torch.tensor(image).float().unsqueeze(0)
         return image
@@ -80,23 +90,24 @@ class Imageprep(torch.utils.data.Dataset):
 # Apply Transformations to Data
 train_set = Imageprep(train_imgs)
 
-pca_data_loader = torch.utils.data.DataLoader(train_set, batch_size=20, shuffle=True)
-fit = False
+pca_data_loader = torch.utils.data.DataLoader(train_set, batch_size=len(train_set), shuffle=True)
+fit = True
+show_explained_variance = True
 print('Preparing data for fitting PCA...')
 for X in pca_data_loader:
     print('Data Shape: ', X.shape, '')
     X = X.view(X.size(0), -1)
     if fit:
-        pca = PCA(n_components=2000)
+        pca = PCA(n_components=300)
         print('Fitting PCA...')
         pca.fit(X.detach().cpu().numpy())
         print('PCA Fitted!')
-        explained_variance = pca.explained_variance_ratio_
+        plot_explained_variance(pca) if show_explained_variance else None
         pickle.dump(pca, open(os.path.join(data_path, 'pca_model.pkl'), 'wb'))
         print('PCA Model Saved in ', os.path.join(data_path, 'pca_model.pkl'))
     else:
         pca = pickle.load(open(os.path.join(data_path, 'pca_model.pkl'), 'rb'))
-    for i in range(6):
+    for i in range(3):
         example_to_show = X[i:i + 1].detach().cpu().numpy()
         og_image = example_to_show.reshape(image_size[0], image_size[1])
         plt.imshow(og_image, cmap='gray')
