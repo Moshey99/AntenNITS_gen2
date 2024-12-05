ef0ae1432b3d395d10a74aa6456f81b44d93b01f
Moshey99 <moshe14199958@gmail.com>
2024-12-06 01:55:06 +0200

    nn_utils.py

--- notebooks/nn_utils.py
+++ notebooks/nn_utils.py
@@ -9,15 +9,18 @@ from inverse_model_evaluate_main import arg_parser, sort_by_metric
 
 if __name__ == "__main__":
     args = arg_parser().parse_args()
-    output_folder = args.output_folder if args.output_folder is not None else os.path.join(args.data_path,
-                                                                                           'checkpoints_inverse')
+    inverse_checkpoint_folder = os.path.join(args.data_path, 'checkpoints_inverse')
+    output_folder_name = args.output_folder_name if args.output_folder_name is not None else 'generated_antennas'
+    output_folder = os.path.join(inverse_checkpoint_folder, output_folder_name)
+    os.makedirs(output_folder, exist_ok=True)
     device = torch.device("cpu")
     data_path = args.data_path
     env_scaler_manager = ScalerManager(path=os.path.join(data_path, 'env_scaler.pkl'))
     env_scaler_manager.try_loading_from_cache()
     ant_scaler_manager = ScalerManager(path=os.path.join(data_path, 'ant_scaler.pkl'))
     ant_scaler_manager.try_loading_from_cache()
-    antenna_dataset_loader = AntennaDataSetsLoader(data_path, batch_size=6949, try_cache=False)
+    antenna_dataset_loader = AntennaDataSetsLoader(data_path, batch_size=6000, try_cache=False)
+    antenna_dataset_loader.load_test_data(args.test_path) if args.test_path is not None else None
     with torch.no_grad():
         all_gamma_stats, all_radiation_stats = [], []
         plot_GT_vs_pred = False
@@ -29,18 +32,17 @@ if __name__ == "__main__":
                                     for i in range(env_trn.shape[0])], device=device)
             break
 
-        for idx, (EMBEDDINGS, GAMMA, RADIATION, ENV, name) in enumerate(antenna_dataset_loader.val_loader):
+        for idx, (EMBEDDINGS, GAMMA, RADIATION, ENV, name) in enumerate(antenna_dataset_loader.tst_loader):
             x_val, gamma_val, rad_val, env_val = ant_scaler_manager.scaler.forward(EMBEDDINGS).float().to(device), \
                 GAMMA.to(device), RADIATION.to(device), ENV.to(device)
             envs_val_og_repr = env_to_dict_representation(env_val)
-            envs_abs_val = torch.tensor([list(model_rel2abs(envs_val_og_repr[i]).values())
+            envs_abs_val = torch.tensor([list(envs_val_og_repr[i].values())
                                          for i in range(env_val.shape[0])], device=device)
             break
         n_neighbors_configs = [10]
         for n_neighbors in n_neighbors_configs:
             nbrs = NearestNeighbors(n_neighbors=n_neighbors, algorithm='auto').fit(env_abs_trn)
             distances, nn_indices = nbrs.kneighbors(envs_abs_val)
-            distances, nn_indices = distances[:, 5:], nn_indices[:, 5:]
             gamma_pred_dB = gamma_trn[nn_indices].squeeze()
             rad_pred = rad_trn[nn_indices].squeeze()
             ant_pred = x_trn[nn_indices].squeeze()
@@ -48,7 +50,7 @@ if __name__ == "__main__":
             all_gamma_stats_best, all_radiation_stats_best = [], []
             for sample_idx in range(nn_indices.shape[0]):
                 sample_name = name[sample_idx]
-                if gamma_val[sample_idx, :int(gamma_val.shape[1] // 2)].min() > -2.5:
+                if gamma_val[sample_idx, :int(gamma_val.shape[1] // 2)].min() > -5:
                     print(f'skipping antenna {sample_name}')
                     continue
                 gamma_stats = produce_gamma_stats(gamma_val[sample_idx].unsqueeze(0), gamma_pred_dB[sample_idx], dataset_type='dB', to_print=False)
@@ -61,16 +63,20 @@ if __name__ == "__main__":
                 ant_neighbor = x_trn[sample_nn_indices[best_nbr_index]]
                 ant_neighbor_inverse = ant_scaler_manager.scaler.inverse(ant_neighbor.unsqueeze(0))
                 ant_neighbor_og_repr = ant_to_dict_representation(ant_neighbor_inverse)[0]
-                env_og_repr = envs_val_og_repr[sample_idx]
-                if check_ant_validity(ant_parameters=ant_neighbor_og_repr, model_parameters=env_og_repr):
+                env_og_rel_repr = env_to_dict_representation(
+                    torch.tensor(np.load(os.path.join(args.test_path, sample_name, 'environment.npy'))[np.newaxis]))[0]
+                if args.repr_mode == 'abs':
+                    # making sure the representation is relative
+                    ant_neighbor_og_repr = ant_abs2rel(ant_neighbor_og_repr, env_og_rel_repr)
+                if check_ant_validity(ant_parameters=ant_neighbor_og_repr, model_parameters=env_og_rel_repr):
                     print('NN is valid for sample:', sample_name)
-                    if sample_idx < 200:
-                        with open(os.path.join(output_folder, f'nn', f'ant_{sample_name}_nn.pickle'),
-                                  'wb') as ant_handle:
-                            pickle.dump(ant_neighbor_og_repr, ant_handle)
-                        with open(os.path.join(output_folder, f'nn', f'env_{sample_name}.pickle'),
-                                  'wb') as env_handle:
-                            pickle.dump(env_og_repr, env_handle)
+                    # if sample_idx < 200:
+                    #     with open(os.path.join(output_folder, f'nn', f'ant_{sample_name}_nn.pickle'),
+                    #               'wb') as ant_handle:
+                    #         pickle.dump(ant_neighbor_og_repr, ant_handle)
+                    #     with open(os.path.join(output_folder, f'nn', f'env_{sample_name}.pickle'),
+                    #               'wb') as env_handle:
+                    #         pickle.dump(env_og_repr, env_handle)
                     all_gamma_stats_best.append(gamma_stats_best)
                     all_radiation_stats_best.append(radiation_stats_best)
                 else:
