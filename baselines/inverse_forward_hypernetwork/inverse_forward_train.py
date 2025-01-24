6d7e910181396fa5635844adf2b655fdeb7858d0
Moshey99 <moshe14199958@gmail.com>
2025-01-24 03:31:15 +0200

    adapt model 6

--- baselines/inverse_forward_hypernetwork/inverse_forward_train.py
+++ baselines/inverse_forward_hypernetwork/inverse_forward_train.py
@@ -17,16 +17,16 @@ import pickle
 def arg_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_path', type=str,
-                default=r'C:\Users\moshey\PycharmProjects\etof_folder_git\AntennaDesign_data\processed_data_130k_200k')
+                default=r"C:\Users\moshey\PycharmProjects\etof_folder_git\AntennaDesign_data\model_6\processed_data")
     parser.add_argument('--forward_checkpoint_path', type=str,
-                        default=r"C:\Users\moshey\PycharmProjects\etof_folder_git\AntennaDesign_data\processed_data_130k_200k\checkpoints\updated_forward_best_dict.pth")
+                        default=r"C:\Users\moshey\PycharmProjects\etof_folder_git\AntennaDesign_data\model_6\processed_data\checkpoints\forward.pth")
     parser.add_argument('--batch_size', type=int, default=12)
     parser.add_argument('--lr', type=float, default=1e-3, help='initial learning rate')
     parser.add_argument('-wd', '--weight_decay', type=float, default=0.001)
     parser.add_argument('--gamma_schedule', type=float, default=0.95, help='gamma decay rate')
     parser.add_argument('--step_size', type=int, default=1, help='step size for gamma decay')
     parser.add_argument('--rad_range', type=list, default=[-15, 5], help='range of radiation values for scaling')
-    parser.add_argument('--geo_weight', type=float, default=1., help='controls the influence of geometry loss')
+    parser.add_argument('--geo_weight', type=float, default=0., help='controls the influence of geometry loss')
     parser.add_argument('--euc_weight', type=float, default=0., help='weight for euclidean loss in GammaRad loss')
     parser.add_argument('--rad_phase_fac', type=float, default=0., help='weight for phase in radiation loss')
     parser.add_argument('--lamda', type=float, default=0.5, help='weight for radiation in gamma radiation loss')
