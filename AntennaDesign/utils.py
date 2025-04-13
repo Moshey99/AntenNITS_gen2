d0a9027e897be0e0f07d4744a939cca9efd216e5
Moshey99 <moshe14199958@gmail.com>
2025-04-14 01:01:18 +0300

    MODEL_TYPE from env variables

--- AntennaDesign/utils.py
+++ AntennaDesign/utils.py
@@ -31,7 +31,9 @@ from ezdxf.addons.drawing import RenderContext, Frontend
 from ezdxf.addons.drawing.matplotlib import MatplotlibBackend
 from shapely.geometry import Polygon
 
-MODEL_TYPE = 6
+MODEL_TYPE = int(os.environ.get("MODEL_TYPE"))
+if MODEL_TYPE is None:
+    raise ValueError("Environment variable 'MODEL_TYPE' is not set.")
 assert MODEL_TYPE in [3, 5, 6], 'MODEL_TYPE must be either 3, 5 or 6.'
 EXAMPLE_FOLDER = os.path.join(Path(__file__).parent, 'EXAMPLE', f'model_{MODEL_TYPE}')
 
